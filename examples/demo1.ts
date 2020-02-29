f3cbbcf71139aa03c316c568ba375c1a534f389f
王嘉巍 <906789088@qq.com>
2020-02-29 16:13:17 +0800

    know object type in ts and find the function return in demo.ts

---
+++ examples/demo1.ts
@@ -0,0 +1,13 @@
+class Person {}
+const teacher: {
+  name: string;
+  age: number;
+} = {
+  name: "Jiawei Wang",
+  age: 23
+};
+const numbers: number[] = [1, 3, 5];
+const Wei: Person = new Person();
+const getTotal = () => number => () => {
+  return 123;
+};

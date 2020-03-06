4a309d8763d581adcff6c81ce8771b310b143589
王嘉巍 <906789088@qq.com>
2020-03-06 16:08:45 +0800

    using node fs and path.resolve module, JSON.parse、JSON.stringify

--- TypeScript/src/crowller.ts
+++ TypeScript/src/crowller.ts
@@ -1,37 +1,64 @@
-import superagent from 'superagent'
-import cheerio from 'cheerio'
+import fs from "fs";
+import path from "path";
+import superagent from "superagent";
+import cheerio from "cheerio";
 interface Course {
-  title: string,
-  count: number
+  title: string;
+  count: number;
+}
+interface CourseResult {
+  time: number;
+  data: Course[];
+}
+interface Content {
+  [propName: number]: Course[];
 }
 class Crowller {
-  private secret = 'secretKey';
+  private secret = "secretKey";
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
   getCourseInfo(html: string) {
-    const $ = cheerio.load(html)
-    const courseItems = $('.course-item');
-    const courseInfos :Course[] = [];
+    const $ = cheerio.load(html);
+    const courseItems = $(".course-item");
+    const courseInfos: Course[] = [];
     courseItems.map((index, element) => {
-      const descs = $(element).find('.course-desc')
+      const descs = $(element).find(".course-desc");
       const title = descs.eq(0).text();
-      const count = parseInt(descs
-        .eq(1)
-        .text()
-        .split('：')[1], 10)
-      courseInfos.push({title, count}) 
-    })
-    const result = {
+      const count = parseInt(
+        descs
+          .eq(1)
+          .text()
+          .split("：")[1],
+        10
+      );
+      courseInfos.push({ title, count });
+    });
+    return {
       time: new Date().getTime(),
       data: courseInfos
-    }
-    console.log(result)
+    };
   }
   async getRawHtml() {
-    const result = await superagent.get(this.url)
-    this.getCourseInfo(result.text)
+    const result = await superagent.get(this.url);
+    return result.text;
+  }
+  generateJsonContent(courseInfo: CourseResult) {
+    const filePath = path.resolve(__dirname, "../data/course.json");
+    let fileContent: Content = {};
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    }
+    fileContent[courseInfo.time] = courseInfo.data;
+    return fileContent;
+  }
+  async initSpiderProcess() {
+    const filePath = path.resolve(__dirname, "../data/course.json");
+    const html = await this.getRawHtml();
+    const courseInfo = this.getCourseInfo(html);
+    const fileContent = this.generateJsonContent(courseInfo);
+    fs.writeFileSync(filePath, JSON.stringify(fileContent));
   }
   constructor() {
-    this.getRawHtml()
+    this.initSpiderProcess();
   }
 }
-const crowller = new Crowller()
+const crowller = new Crowller();

bee05826b060b18d775ac37757788efd05e3138b
王嘉巍 <906789088@qq.com>
2020-03-20 11:24:00 +0800

    using namespace and decalre function to void

--- TSConfig/src/jquery.d.ts
+++ TSConfig/src/jquery.d.ts
@@ -3,8 +3,23 @@
 
 //定义全局函数
 interface JqueryInstance {
-  html: (html: string) => {};
+  html: (html: string) => JqueryInstance;
 }
+//函数重载
 declare function $(readyFunc: () => void): void;
-
 declare function $(selector: string): JqueryInstance;
+
+//如何对对象进行类型定义，以及对类进行类型定义，以及命名空间的嵌套
+declare namespace $ {
+  namespace fn {
+    class init {}
+  }
+}
+
+//使用 interface的语法，实现函数的重载
+// interface JQuery {
+//   (readyFunc: () => void): void;
+//   (selector: string): JqueryInstance;
+// }
+
+// declare var $: JQuery;

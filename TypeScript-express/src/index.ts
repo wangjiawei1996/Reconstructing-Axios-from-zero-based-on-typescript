68a69dccf579580fbd3367df3ab81c5913a5703a
王嘉巍 <906789088@qq.com>
2020-03-25 11:24:29 +0800

    using ts write exporess node and using middlewhere body-Parser

--- TypeScript-express/src/index.ts
+++ TypeScript-express/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 import router from "./router";
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(router);
 app.listen(7001, () => {
--- TypeScript-express/src/router.ts
+++ TypeScript-express/src/router.ts
@@ -5,14 +5,27 @@ import DellAnalyzer from "./dellAnalyzer";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("hello world");
+  res.send(`
+    <html>
+      <body>
+        <form method='post' action='/getData'>
+          <input type="password" name="password" />
+          <button>提交</button>
+        </form>
+      </body>
+    </html>
+  `);
 });
 
-router.get("/getData", (req: Request, res: Response) => {
-  const secret = "secretKey";
-  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
-  const analyzer = DellAnalyzer.getInstance();
-  new Crowller(url, analyzer);
-  res.send("bye world");
+router.post("/getData", (req: Request, res: Response) => {
+  if (req.body.password === "123") {
+    const secret = "secretKey";
+    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+    const analyzer = DellAnalyzer.getInstance();
+    new Crowller(url, analyzer);
+    res.send("Hello world");
+  } else {
+    res.send("password error");
+  }
 });
 export default router;

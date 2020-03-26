04bf4cb1198e47475eca3a5f7aec4093aaebfff8
王嘉巍 <906789088@qq.com>
2020-03-26 17:46:03 +0800

    import cookieSession in index.ts and create url login logout this path and fs read file

--- TypeScript-express/src/index.ts
+++ TypeScript-express/src/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response, NextFunction } from "express";
+import cookieSession from "cookie-session";
 import bodyParser from "body-parser";
 import router from "./router";
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use((req: Request, res: Response, next: NextFunction) => {
-  req.teacherName = "Jiawei";
-  next();
-});
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["teacher Jiawei"],
+    maxAge: 24 * 60 * 60 * 1000
+  })
+);
 app.use(router);
-app.listen(7001, () => {
+app.listen(7002, () => {
   console.log("server is running");
 });
--- TypeScript-express/src/router.ts
+++ TypeScript-express/src/router.ts
@@ -1,7 +1,8 @@
 import { Router, Request, Response } from "express";
 import Crowller from "./crowller";
 import DellAnalyzer from "./dellAnalyzer";
-
+import fs from "fs";
+import path from "path";
 interface RequestWithBody extends Request {
   body: {
     [key: string]: string | undefined;
@@ -11,28 +12,74 @@ interface RequestWithBody extends Request {
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send(`
-    <html>
-      <body>
-        <form method='post' action='/getData'>
-          <input type="password" name="password" />
-          <button>提交</button>
-        </form>
-      </body>
-    </html>
-  `);
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    res.send(`
+      <html>
+        <body>
+          <a href='/logout'>退出</a>
+          <a href='/getData'>爬取内容</a>
+          <a href='/showData'>展示内容</a>
+        </body>
+      </html>
+    `);
+  } else {
+    res.send(`
+      <html>
+        <body>
+          <form method='post' action='/login'>
+            <input type="password" name="password" />
+            <button>登录</button>
+          </form>
+        </body>
+      </html>
+    `);
+  }
 });
-
-router.post("/getData", (req: RequestWithBody, res: Response) => {
+router.get("/logout", (req: Request, res: Response) => {
+  if (req.session) {
+    req.session.login = undefined;
+  }
+  res.redirect("/");
+});
+router.post("/login", (req: RequestWithBody, res: Response) => {
   const { password } = req.body;
-  if (password === "123") {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    res.send("已经登录过");
+  } else {
+    if (password === "123" && req.session) {
+      req.session.login = true;
+      res.send("登录成功");
+    } else {
+      res.send("登录失败");
+    }
+  }
+});
+router.get("/getData", (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
     const secret = "secretKey";
     const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
     const analyzer = DellAnalyzer.getInstance();
     new Crowller(url, analyzer);
     res.send("Hello world");
   } else {
-    res.send(`${req.teacherName}password error`);
+    res.send("请登录后爬取");
+  }
+});
+router.get("/showData", (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    try {
+      const position = path.resolve(__dirname, "../data/course.json");
+      const result = fs.readFileSync(position, "utf 8");
+      res.json(JSON.parse(result));
+    } catch {
+      res.send("尚未爬取到内容");
+    }
+  } else {
+    res.send("请登陆后爬取内容");
   }
 });
 export default router;

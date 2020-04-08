1f50fb6829189bc2f5e5e0d4c9e267abbf8fca12
王嘉巍 <906789088@qq.com>
2020-04-08 11:15:38 +0800

    decorator param in class and catch paramIndex in method

--- superTypeScript/src/index.ts
+++ superTypeScript/src/index.ts
@@ -1,31 +1,13 @@
-// function testDecorator(flag: boolean) {
-//   if (flag) {
-//     return function(constructor: any) {
-//       constructor.prototype.getName = () => {
-//         console.log("welcome Alipay");
-//       };
-//     };
-//   } else {
-//     return function(constructor: any) {};
-//   }
-// }
-
-// @testDecorator(true)
-// class Test {}
-
-// const test = new Test();
-// (test as any).getName();
-
-//修改的并不是实例上的name,而是原型上的name
-function nameDecorator(target: any, key: string): any {
-  target[key] = "wang";
+//原型、方法名、参数所在的位置
+function paramDecorator(target: any, key: string, paramIndex: number): any {
+  console.log(target, key, paramIndex);
 }
 
-//name放在实例上
 class Test {
-  @nameDecorator
-  name = "Jiawei";
+  getinfo(@paramDecorator name: string, age: number) {
+    console.log(name, age);
+  }
 }
 
 const test = new Test();
-console.log((test as any).__proto__.name);
+console.log(test.getinfo("123", 24));

d2924326c5b6a6a99000e9adc97e593037341e92
王嘉巍 <906789088@qq.com>
2020-05-19 17:08:02 +0800

    using antd and create form to submit some data

--- fontend-project/src/App.tsx
+++ fontend-project/src/App.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import "./login.css";
 
-function App() {
-  return <div> Hello world</div>;
-}
+const NormalLoginForm = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
 
-export default App;
+  return (
+    <div className="login-page">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "请输入登录密码"
+            }
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default NormalLoginForm;
